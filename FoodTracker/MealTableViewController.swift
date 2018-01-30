739ce7a32a6598981c2877c81ba53353c3b71baf
Navya Paladugu <navyasaip@gmail.com>
2018-01-30 13:18:07 -0500

    Persist Data
    Last part of the lesson

--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -21,8 +21,14 @@ class MealTableViewController: UITableViewController {
         // Use the edit button item provided by the table view controller.
         navigationItem.leftBarButtonItem = editButtonItem
         
-        // Load the sample data
-        loadSampleMeals()
+        // Load any saved meals, otherwise load sample data.
+        if let savedMeals = loadMeals(){
+            meals += savedMeals
+        }
+        else {
+            // Load the sample data
+            loadSampleMeals()
+        }
     }
     
     override func didReceiveMemoryWarning() {
@@ -76,6 +82,7 @@ class MealTableViewController: UITableViewController {
         if editingStyle == .delete {
             // Delete the row from the data source
             meals.remove(at: indexPath.row)
+            saveMeals()
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
@@ -137,6 +144,7 @@ class MealTableViewController: UITableViewController {
     // MARK: Actions
     
     @IBAction func unwindToMealList(sender: UIStoryboardSegue){
+        
         if let sourceViewController = sender.source as? MealViewController, let meal = sourceViewController.meal{
             
             if let selectedIndexPath = tableView.indexPathForSelectedRow{
@@ -151,6 +159,9 @@ class MealTableViewController: UITableViewController {
                 meals.append(meal)
                 tableView.insertRows(at: [newIndexPath], with: .automatic)
             }
+            
+            // Save the Meals
+            saveMeals()
         }
     }
     
@@ -178,6 +189,19 @@ class MealTableViewController: UITableViewController {
         // Adding Meal objects to the array
         meals += [meal1, meal2, meal3]
         
+    }
+    
+    private func saveMeals(){
+        let isSuccessfulSave = NSKeyedArchiver.archiveRootObject(meals, toFile: Meal.ArchiveURL.path)
+        if isSuccessfulSave {
+            os_log("Meals successfully saved.", log: OSLog.default, type: .debug)
+        } else {
+            os_log("Failed to save meals...", log: OSLog.default, type: .error)
+        }
+    }
+    
+    private func loadMeals() -> [Meal]? {
+        return NSKeyedUnarchiver.unarchiveObject(withFile: Meal.ArchiveURL.path) as? [Meal]
     }
     
 }
